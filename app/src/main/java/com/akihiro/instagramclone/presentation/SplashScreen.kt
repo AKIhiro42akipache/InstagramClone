7ef065844800a3c5c78b56c1977d940025f0d6c0
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-11-19 23:49:46 +0900

    SplashScreenとViewModelの繋ぎこみを行い指定秒後ログイン画面に遷移するように設定

--- app/src/main/java/com/akihiro/instagramclone/presentation/SplashScreen.kt
+++ app/src/main/java/com/akihiro/instagramclone/presentation/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.akihiro.instagramclone.R
@@ -50,8 +52,11 @@ fun SplashScreen(navController: NavController, authViewModel: AuthenticationView
         }
     }
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         contentAlignment = Alignment.Center,
+
     ) {
         Image(
             modifier = Modifier.scale(scale.value),
